447742ff307a45a74c4ca31c86b09cddcf8ac579
Daniel Hannus <prozeyin.co@gmail.com>
2024-04-29 13:51:32 +0300

    Update PawnTests.java

--- src/test/java/ax/ha/tdd/chess/engine/PawnTests.java
+++ src/test/java/ax/ha/tdd/chess/engine/PawnTests.java
@@ -6,8 +6,7 @@ import ax.ha.tdd.chess.engine.pieces.Pawn;
 import ax.ha.tdd.chess.engine.pieces.PieceType;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PawnTests {
 
@@ -24,5 +23,45 @@ public class PawnTests {
         //For debugging, you can print the board to console, or if you want
         //to implement a command line interface for the game
         System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+    @Test
+    public void pawnMovesForwardOneSquare() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
+        chessboard.addPiece(pawn);
+
+        assertTrue(pawn.canMove(chessboard, new Square("e3")));
+    }
+
+    @Test
+    public void pawnMovesForwardTwoSquaresFromStart() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
+        chessboard.addPiece(pawn);
+
+        assertTrue(pawn.canMove(chessboard, new Square("e4")));
+    }
+
+    @Test
+    public void pawnCapturesDiagonally() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
+        ChessPiece enemyPiece = new Pawn(Color.BLACK, new Square("d3"));
+        chessboard.addPiece(pawn);
+        chessboard.addPiece(enemyPiece);
+
+        assertTrue(pawn.canMove(chessboard, new Square("d3")));
+    }
+
+    @Test
+    public void pawnBlockedByAnotherPiece() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
+        ChessPiece blockingPiece = new Pawn(Color.WHITE, new Square("e3"));
+        chessboard.addPiece(pawn);
+        chessboard.addPiece(blockingPiece);
+
+        assertFalse(pawn.canMove(chessboard, new Square("e3")));
     }
 }
