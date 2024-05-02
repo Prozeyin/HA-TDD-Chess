a12e40f24ffb3894fcb60c674f36eb6e86f011ca
Daniel Hannus <prozeyin.co@gmail.com>
2024-05-02 20:51:36 +0300

    Update Chessboard.java

--- src/main/java/ax/ha/tdd/chess/engine/Chessboard.java
+++ src/main/java/ax/ha/tdd/chess/engine/Chessboard.java
@@ -2,6 +2,8 @@ package ax.ha.tdd.chess.engine;
 
 import ax.ha.tdd.chess.engine.pieces.ChessPiece;
 
+import java.util.List;
+
 public interface Chessboard extends Iterable<ChessPiece[]>{
 
     ChessPiece getPieceAt(final Square square);
@@ -9,4 +11,9 @@ public interface Chessboard extends Iterable<ChessPiece[]>{
     void addPiece(final ChessPiece chessPiece);
 
     void removePieceAt(final Square square);
+
+    List<ChessPiece> getPieces();
+
+    boolean isKingInCheck(Color kingColor);
+
 }
